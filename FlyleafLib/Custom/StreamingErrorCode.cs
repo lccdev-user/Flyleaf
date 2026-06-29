858e1b8cbdd7167b7bfb6a6fd45a52e0dcec3199
Alexander Rempel <a.rempel@pke-de.com>
2026-06-29 15:42:08 +0200

    - ICustomVideoStream neue Methode ErrorByStreamingDetected
    - Demuxer - AVERROR _EXIT beim vorwärts Play

---
+++ FlyleafLib/Custom/StreamingErrorCode.cs
@@ -0,0 +1,8 @@
+﻿
+public static class StreamingErrorCode
+{
+    public static int None = 0;
+    public static int DemuxerError = 1;
+    public static int DecoderError = 2;
+    public static int FrameOrderError = 3; 
+}
